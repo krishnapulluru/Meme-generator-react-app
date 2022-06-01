bed49177dd224705141c5dcea5ebffb0e8c7cd3e
krishna Pulluru <krishnapulluru@getninesoft.com>
2022-06-01 23:16:15 +0530

    Removed Pract.js from componenets

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Header from "./components/Header";
 import Meme from "./components/Meme";
-import Practice from "./components/Pract";
 import "./App.css";
 
 function App() {
@@ -11,8 +10,6 @@ function App() {
 			<section>
 				<Meme />
 			</section>
-
-			{/* <Practice /> */}
 		</div>
 	);
 }
